088d066af2ad28b9447b103f44f3b397b3bfb2dd
Dan Pickett <dan.pickett@launchware.com>
2014-06-27 17:49:09 -0400

    syntax issue

--- osx/la_cookbooks/fueled-station/recipes/oh-my-zsh.rb
+++ osx/la_cookbooks/fueled-station/recipes/oh-my-zsh.rb
@@ -1,6 +1,6 @@
 script "oh-my-zsh install from github" do
   interpreter "bash"
-  url = https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh
+  url = "https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh"
   code <<-EOS
     curl -sLf #{url} -o - | sh
     chsh -s /bin/zsh
