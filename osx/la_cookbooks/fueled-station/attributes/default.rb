16cf3fbb17ea420f52fb81534631f1a4a5362c5a
Dan Pickett <dan.pickett@launchware.com>
2014-06-27 17:59:33 -0400

    straighten out attributes file

--- osx/la_cookbooks/fueled-station/attributes/default.rb
+++ osx/la_cookbooks/fueled-station/attributes/default.rb
@@ -1,9 +1,9 @@
-node.default['rocket-fuel']['chruby']['rubies']['ruby'] = [
+default['rocket-fuel']['chruby']['rubies']['ruby'] = [
   '1.9.3',
   '2.0',
   '2.1.2'
 ]
 
-node.default['rocket-fuel']['chruby']['default_ruby'] = '2.1.2'
+default['rocket-fuel']['chruby']['default_ruby'] = '2.1.2'
 
-node.default['ohmyzsh']['theme'] = 're5et'
+default['roket-fuel']['ohmyzsh']['theme'] = 're5et'
