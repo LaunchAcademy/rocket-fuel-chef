33c38c285162f5605ef32d7ace98f4f95cfca68d
Dan Pickett <dan.pickett@launchware.com>
2014-06-27 17:19:14 -0400

    chruby setup

--- osx/la_cookbooks/fueled-station/recipes/chruby.rb
+++ osx/la_cookbooks/fueled-station/recipes/chruby.rb
@@ -2,3 +2,28 @@ include_recipe 'homebrew'
 
 package 'chruby'
 package 'ruby-install'
+package 'openssl'
+
+#install desired ruby versions
+node['rocket-fuel']['chruby']['rubies'].each do |vm, versions|
+  versions.each do |version|
+    execute "install ruby #{vm}: #{version}" do
+      command "ruby-install #{vm} #{version}"
+      not_if { Dir.exist?(File.join(ENV['HOME']), [vm, version].join('-')) }
+    end
+  end
+end
+
+#set default ruby version
+file ::File.join(ENV['HOME'], '.ruby-version') do
+  content node['rocket-fuel']['chruby']['default_ruby']
+  user node['current_user']
+  action :create_if_missing
+end
+
+#don't install rdoc or ri documentation
+file ::File.join(ENV['HOME'], '.gemrc') do
+  content "gem: --no-rdoc --no-ri"
+  user node['current_user']
+  action :create_if_missing
+end
