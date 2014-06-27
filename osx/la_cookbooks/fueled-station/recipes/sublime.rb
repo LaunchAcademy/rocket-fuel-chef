276964738d67df7ef824a8471d0b5a4872669ac4
Dan Pickett <dan.pickett@launchware.com>
2014-06-27 17:41:06 -0400

    set sublime to the default

--- osx/la_cookbooks/fueled-station/recipes/sublime.rb
+++ osx/la_cookbooks/fueled-station/recipes/sublime.rb
@@ -1,3 +1,5 @@
+include_package 'rocket-fuel::oh-my-zsh'
+
 dmg_package 'Sublime Text' do
   volumes_dir 'Sublime Text'
   source 'http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%20Build%203059.dmg'
@@ -10,6 +12,12 @@ end
 
 template File.join(ENV['HOME'], 'Library/Application Support Sublime Text 3/Packages/User/Preferences.sublime-settings') do
   source "Preferences.sublime-settings.erb"
+  owner node['current_user']
+  action :create_if_missing
+end
+
+file File.join(ENV['HOME'], template ::File.join(ENV['HOME'], 'oh-my-zsh/custom/sublime.zsh') do
+  contents 'export EDITOR="subl -w"'
   owner node['current_user']
   action :create_if_missing
 end
