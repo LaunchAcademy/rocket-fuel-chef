7a1f0a01fac87507d0585f5bd780e6b65c84c88d
Dan Pickett <dan.pickett@launchware.com>
2014-06-27 18:32:53 -0400

    set sublime straight

--- osx/la_cookbooks/fueled-station/recipes/sublime.rb
+++ osx/la_cookbooks/fueled-station/recipes/sublime.rb
@@ -1,7 +1,8 @@
-include_package 'fueled-station::oh-my-zsh'
+include_recipe 'fueled-station::oh-my-zsh'
 
 dmg_package 'Sublime Text' do
   volumes_dir 'Sublime Text'
+  destination File.join(ENV['HOME'], 'Applications')
   source 'http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%20Build%203059.dmg'
   action :install
 end
@@ -10,14 +11,20 @@ link "/usr/local/bin/subl" do
   to "~/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl"
 end
 
-template File.join(ENV['HOME'], 'Library/Application Support Sublime Text 3/Packages/User/Preferences.sublime-settings') do
+directory File.join(ENV['HOME'], 'Library/Application Support/Sublime Text 3') do
+  owner node['current_user']
+  action :create
+end
+
+template File.join(ENV['HOME'], 'Library/Application Support/Sublime Text 3/Packages/User/Preferences.sublime-settings') do
   source "Preferences.sublime-settings.erb"
   owner node['current_user']
   action :create_if_missing
 end
 
-file File.join(ENV['HOME'], template ::File.join(ENV['HOME'], 'oh-my-zsh/custom/sublime.zsh') do
-  contents 'export EDITOR="subl -w"'
+file File.join(ENV['HOME'], '.oh-my-zsh/custom/sublime.zsh') do
+  owner node['current_user']
+  content 'export EDITOR="subl -w"'
   owner node['current_user']
   action :create_if_missing
 end
