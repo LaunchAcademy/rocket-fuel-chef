f1f333e82388dc14a3b67143ae588d4b3ca92f49
Dan Pickett <dan.pickett@launchware.com>
2014-06-27 21:45:29 -0400

    handle for absolute paths

--- osx/la_cookbooks/fueled-station/libraries/mkdir_p.rb
+++ osx/la_cookbooks/fueled-station/libraries/mkdir_p.rb
@@ -2,7 +2,9 @@ class Chef::Recipe
   def mkdir_p(directory, &block)
     directories = directory.split("/")
     directories.each_with_index do |dir, index|
-      directory(File.join(directories[0..index], &block))
+      if dir != ''
+        directory(File.join(directories[0..index], &block))
+      end
     end
   end
 end
