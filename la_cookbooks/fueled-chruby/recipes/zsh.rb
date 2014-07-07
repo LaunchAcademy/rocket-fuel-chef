d9aae61ada50ba7f495fe665977bef233cfea758
Dan Pickett <dan.pickett@launchware.com>
2014-07-07 17:56:25 -0400

    namespace zsh install check

--- la_cookbooks/fueled-chruby/recipes/zsh.rb
+++ la_cookbooks/fueled-chruby/recipes/zsh.rb
@@ -2,5 +2,5 @@ template ::File.join(ENV['HOME'], '.oh-my-zsh/custom/chruby.zsh') do
   source 'chruby.zsh.erb'
   owner node['current_user']
   action :create_if_missing
-  only_if { ohmyzsh_installed? }
+  only_if { FueledZsh.installed? }
 end
--- la_cookbooks/fueled-sublime/recipes/zsh.rb
+++ la_cookbooks/fueled-sublime/recipes/zsh.rb
@@ -3,5 +3,5 @@ file File.join(ENV['HOME'], '.oh-my-zsh/custom/sublime.zsh') do
   content 'export EDITOR="subl -w"'
   owner node['current_user']
   action :create_if_missing
-  only_if { ohmyzsh_installed? }
+  only_if { FueledZsh.installed? }
 end
