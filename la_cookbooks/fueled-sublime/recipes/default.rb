84f69097befeda9f59662964d324f510cd60e163
Dan Pickett <dan.pickett@launchware.com>
2014-07-08 16:01:51 -0400

    sublime packages

--- la_cookbooks/fueled-sublime/recipes/default.rb
+++ la_cookbooks/fueled-sublime/recipes/default.rb
@@ -1,4 +1,5 @@
 include_recipe 'fueled-sublime::install'
 include_recipe 'fueled-sublime::cmd-line-shortcut'
 include_recipe 'fueled-sublime::preferences'
+include_recipe 'fueled-sublime::packages'
 include_recipe 'fueled-sublime::zsh'
