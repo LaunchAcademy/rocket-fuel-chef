5cc7734d3a3f1a11d94f045361ec38d8dd71f299
Dan Pickett <dan.pickett@launchware.com>
2014-07-14 16:18:32 -0400

    manually install chruby and ruby-install on linux

--- la_cookbooks/fueled-chruby/recipes/chruby.rb
+++ la_cookbooks/fueled-chruby/recipes/chruby.rb
@@ -1,3 +1,20 @@
-include_recipe 'homebrew'
+if platform_family?('mac_os_x')
+  include_recipe 'homebrew'
+  package 'chruby'
+else
+  chruby_path = "#{Chef::Config[:file_cache_path]}/chruby.tar.gz"
+  remote_file chruby_path do
+    source "https://github.com/postmodern/chruby/archive/v0.3.8.tar.gz"
+    action :create_if_missing
+  end
 
-package 'chruby'
+  execute 'extract chruby' do
+    command "tar -xzvf #{chruby_path}"
+  end
+
+  require 'chef-sudo'
+  sudo 'install chruby' do
+    user root
+    command "cd #{Chef::Config[:file_cache_path]}/chruby* && make install"
+  end
+end
--- la_cookbooks/fueled-chruby/recipes/ruby-install.rb
+++ la_cookbooks/fueled-chruby/recipes/ruby-install.rb
@@ -1,2 +1,20 @@
-package 'openssl'
-package 'ruby-install'
+if platform_family?('mac_os_x')
+  package 'openssl'
+  package 'ruby-install'
+else
+  install_path = "#{Chef::Config[:file_cache_path]}/ruby-install.tar.gz"
+  remote_file chruby_path do
+    source "https://github.com/postmodern/ruby-install/archive/v0.4.3.tar.gz"
+    action :create_if_missing
+  end
+
+  execute 'extract ruby install' do
+    command "tar -xzvf #{install_path}"
+  end
+
+  require 'chef-sudo'
+  sudo 'install chruby' do
+    user root
+    command "cd #{Chef::Config[:file_cache_path]}/ruby-install* && make install"
+  end
+end
