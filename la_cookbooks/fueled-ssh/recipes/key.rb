bee1c3d63bd73bae1385f7cbf20647c5d02d490e
Dan Pickett <dan.pickett@launchware.com>
2014-07-14 17:38:33 -0400

    tweaks to SSH recipes

--- la_cookbooks/fueled-ssh/recipes/key.rb
+++ la_cookbooks/fueled-ssh/recipes/key.rb
@@ -2,5 +2,5 @@ pub_to_check = File.join(ENV['HOME'], ".ssh/id_rsa.pub")
 
 execute 'create passwordless key' do
   command 'ssh-keygen -b 2048 -t rsa -f #{pub_to_check} -q -N ""'
-  only_if { FileTest.exists?(pub_to_check)}
+  only_if { !FileTest.exists?(pub_to_check)}
 end
