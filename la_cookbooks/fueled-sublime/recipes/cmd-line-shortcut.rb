7da2a0cb470c81449c257162c64d44dc7019455b
Dan Pickett <dan.pickett@launchware.com>
2014-07-14 22:26:12 -0400

    first pass at sublime text 3 linux support

--- la_cookbooks/fueled-sublime/recipes/cmd-line-shortcut.rb
+++ la_cookbooks/fueled-sublime/recipes/cmd-line-shortcut.rb
@@ -1,3 +1,16 @@
-link "/usr/local/bin/subl" do
-  to File.join(ENV['HOME'], "Applications/Sublime Text.app/Contents/SharedSupport/bin/subl")
+if platform_family?('mac_os_x')
+  link "/usr/local/bin/subl" do
+    to File.join(ENV['HOME'], "Applications/Sublime Text.app/Contents/SharedSupport/bin/subl")
+  end
+else
+  require 'chef-sudo'
+  sudo 'link sublime' do
+    user 'root'
+    command 'ln -s /opt/sublime_text_3/sublime_text /usr/bin/sublime'
+  end
+
+  sudo 'link subl' do
+    user 'root'
+    command 'ln -s /opt/sublime_text_3/sublime_text /usr/bin/subl'
+  end
 end
