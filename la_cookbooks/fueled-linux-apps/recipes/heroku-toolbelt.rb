03d4647bee0f2d7ab5a431e1236b3bc10a7aaf50
Dan Pickett <dan.pickett@launchware.com>
2014-07-15 09:26:24 -0400

    syntax error in heroku tool belt recipe

--- la_cookbooks/fueled-linux-apps/recipes/heroku-toolbelt.rb
+++ la_cookbooks/fueled-linux-apps/recipes/heroku-toolbelt.rb
@@ -2,7 +2,7 @@ execute 'install heroku toolbelt' do
   command '\curl -sSL https://toolbelt.heroku.com/install.sh | bash -s stable'
 end
 
-template File.join(ENV['HOME'], 'oh-my-zsh/custom/heroku-toolbelt.zsh' do
+template File.join(ENV['HOME'], 'oh-my-zsh/custom/heroku-toolbelt.zsh') do
   source 'heroku-toolbelt.zsh.erb'
   owner node['current_user']
   action :create_if_missing
