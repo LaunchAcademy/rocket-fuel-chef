003998caff1707ad845423b8e35b07f7435fb916
Dan Pickett <dan.pickett@launchware.com>
2014-07-16 21:16:36 -0400

    use double quotes to allow interpolation

--- la_cookbooks/fueled-zsh/recipes/replace-shell.rb
+++ la_cookbooks/fueled-zsh/recipes/replace-shell.rb
@@ -6,6 +6,6 @@ end
 require 'chef-sudo'
 zshrc_path = File.join(ENV['HOME'], '.zshrc')
 sudo 'ensure .zshrc has correct owner' do
-  command 'chown $USER #{zshrc_path}'
+  command "chown $USER #{zshrc_path}"
   only_if { FileTest.exists?(zshrc_path) }
 end
