d1ae2ec96329d5a8117662d3e136bcf55263470c
Dan Pickett <dan.pickett@launchware.com>
2014-09-19 16:40:04 -0400

    linter package is currently throwing an error
    omit it for now

--- la_cookbooks/fueled-atom/recipes/default.rb
+++ la_cookbooks/fueled-atom/recipes/default.rb
@@ -5,7 +5,7 @@ include_recipe 'fueled-atom::rails'
 include_recipe 'fueled-atom::colorpicker'
 include_recipe 'fueled-atom::rails-rspec'
 
-include_recipe 'fueled-atom::linter'
+# include_recipe 'fueled-atom::linter'
 include_recipe 'fueled-atom::linter-ruby'
 include_recipe 'fueled-atom::linter-jshint'
 
