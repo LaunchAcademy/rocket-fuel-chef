8f2c4822c1dc27caeb0e75b59828420a5202fc72
Dan Pickett <dan.pickett@launchware.com>
2014-09-19 16:53:48 -0400

    omit atom packages altogether for now
    apm is responding with an error

--- cookbooks/fueled-atom/recipes/default.rb
+++ cookbooks/fueled-atom/recipes/default.rb
@@ -1,9 +1,9 @@
 include_recipe 'fueled-atom::install'
 include_recipe 'fueled-atom::fix_perms'
 
-include_recipe 'fueled-atom::rails'
-include_recipe 'fueled-atom::colorpicker'
-include_recipe 'fueled-atom::rails-rspec'
+# include_recipe 'fueled-atom::rails'
+# include_recipe 'fueled-atom::colorpicker'
+# include_recipe 'fueled-atom::rails-rspec'
 
 # include_recipe 'fueled-atom::linter'
 # include_recipe 'fueled-atom::linter-ruby'
