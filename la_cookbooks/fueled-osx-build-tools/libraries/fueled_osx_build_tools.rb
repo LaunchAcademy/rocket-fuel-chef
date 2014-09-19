96ed7d907af26b57ce82dd951d2f91d93f701bed
Dan Pickett <dan.pickett@launchware.com>
2014-09-19 12:19:04 -0400

    system ruby (1.8.7) does not like require_relative

--- la_cookbooks/fueled-osx-build-tools/libraries/fueled_osx_build_tools.rb
+++ la_cookbooks/fueled-osx-build-tools/libraries/fueled_osx_build_tools.rb
@@ -1,9 +1,9 @@
-require_relative 'fueled_osx_build_tools/download'
-require_relative 'fueled_osx_build_tools/os_check'
+require File.join(File.dirname(__FILE__), 'fueled_osx_build_tools/download')
+require File.join(File.dirname(__FILE__), 'fueled_osx_build_tools/os_check')
 
-require_relative 'fueled_osx_build_tools/download_routine'
-require_relative 'fueled_osx_build_tools/dmg_metadata'
-require_relative 'fueled_osx_build_tools/clt_installer'
+require File.join(File.dirname(__FILE__), 'fueled_osx_build_tools/download_routine')
+require File.join(File.dirname(__FILE__), 'fueled_osx_build_tools/dmg_metadata')
+require File.join(File.dirname(__FILE__), 'fueled_osx_build_tools/clt_installer')
 
 module FueledOsxBuildTools
 end
