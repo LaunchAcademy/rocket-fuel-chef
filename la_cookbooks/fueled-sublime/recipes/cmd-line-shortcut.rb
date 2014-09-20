5dd9c5e5be34418e0dec5e632c5d57bc91b275d8
Dan Pickett <dan.pickett@launchware.com>
2014-09-20 09:06:23 -0400

    fix typo
    this is why you should not code before coffee

--- la_cookbooks/fueled-sublime/recipes/cmd-line-shortcut.rb
+++ la_cookbooks/fueled-sublime/recipes/cmd-line-shortcut.rb
@@ -2,7 +2,7 @@ if platform_family?('mac_os_x')
   link "/usr/local/bin/subl" do
     to File.join(ENV['HOME'], "Applications/Sublime Text.app/Contents/SharedSupport/bin/subl")
   end
-elsif !platform_famly?('windows')
+elsif !platform_family?('windows')
   execute 'link sublime' do
     user 'root'
     command 'ln -sf /opt/sublime_text_3/sublime_text /usr/bin/sublime'
