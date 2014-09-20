afa01e154e2ac03d8cfe783f810d60abdf3fa0b5
Dan Pickett <dan.pickett@launchware.com>
2014-09-20 09:23:19 -0400

    chown isn’t necessary on windows builds

--- la_cookbooks/fueled-atom/recipes/fix_perms.rb
+++ la_cookbooks/fueled-atom/recipes/fix_perms.rb
@@ -8,4 +8,5 @@ end
 execute 'ensure ownership of the atom root for preferences' do
   user 'root'
   command 'chown -R ' + node['current_user'] +':staff "' + atom_root + '"'
+  not_if { platform_family?('windows') }
 end
