0bf4023bca1b2b43fd42beabeaeb45e9ecc2cd63
Dan Pickett <dan.pickett@launchware.com>
2014-12-19 16:30:17 -0500

    postgres.app bumped a minor

--- cookbooks/fueled-osx-apps/recipes/postgres.rb
+++ cookbooks/fueled-osx-apps/recipes/postgres.rb
@@ -3,7 +3,7 @@ homebrew_cask 'postgres'
 #when you open postgres.app, it will ask to move to /Applications, so assume that
 file File.join(ENV['HOME'], '.oh-my-zsh/custom/postgres.zsh') do
   owner ENV['SUDO_USER'] || node['current_user']
-  content "export PATH=\"#{ENV['HOME']}/Applications/Postgres.app/Contents/Versions/9.3/bin\":$PATH"
+  content "export PATH=\"#{ENV['HOME']}/Applications/Postgres.app/Contents/Versions/9.4/bin\":$PATH"
   action :create_if_missing
   only_if { FueledZsh.installed? }
 end
