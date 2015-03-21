5628125e6d9cc47e90c6a69b3181809bd073c473
Dan Pickett <dan.pickett@launchware.com>
2015-03-21 14:03:10 -0400

    change cask location to /Applications

--- la_cookbooks/fueled-osx-apps/recipes/cask.rb
+++ la_cookbooks/fueled-osx-apps/recipes/cask.rb
@@ -12,11 +12,24 @@ mkdir_p '/opt/homebrew-cask/CaskRoom' do
   action :create
 end
 
-app_dir = "#{ENV['HOME']}/Applications"
+app_dir = "/Applications"
 
 mkdir_p app_dir do
   owner ENV['SUDO_USER'] || node['current_user']
   action :create
+end
+
+cask_opts = "--appdir=/Applications"
+
+env 'HOMEBREW_CASK_OPTS' do
+  value cask_opts
+end
+
+file ::File.join(ENV['HOME'], '.oh-my-zsh/custom/cask.zsh') do
+  content 'export HOMEBREW_CASK_OPTS="--appdir=/Applications"'
+  owner ENV['SUDO_USER'] || node['current_user']
+  action :create_if_missing
+  only_if { FueledZsh.installed? }
 end
 
 execute 'fix caskroom regression' do
@@ -24,6 +37,3 @@ execute 'fix caskroom regression' do
     ' /opt/homebrew-cask/CaskRoom'
 end
 
-execute 'fix ~/Applications perms' do
-  command 'chown -R ' + (ENV['SUDO_USER'] || node['current_user']) + ' ' + app_dir
-end
