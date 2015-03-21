f01f21f19af662ef887f4c7239017aec80828184
Dan Pickett <dan.pickett@launchware.com>
2015-03-21 15:36:27 -0400

    update ruby versions that are installed

--- la_cookbooks/fueled-chruby/attributes/default.rb
+++ la_cookbooks/fueled-chruby/attributes/default.rb
@@ -1,7 +1,7 @@
 default['rocket-fuel']['chruby']['rubies']['ruby'] = [
   '1.9.3-p551',
   '2.0.0-p598',
-  '2.1.5'
+  '2.2.1'
 ]
 
-default['rocket-fuel']['chruby']['default_ruby'] = '2.1.4'
+default['rocket-fuel']['chruby']['default_ruby'] = '2.2.1'
