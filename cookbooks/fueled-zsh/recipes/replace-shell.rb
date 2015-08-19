53cbcfb9c732597ae857e1c1cef4524fccf674c2
Dan Pickett <dan.pickett@launchacademy.com>
2015-08-18 20:09:47 -0400

    fix order and attempt to access SUDO_USER env var

--- cookbooks/fueled-zsh/recipes/replace-shell.rb
+++ cookbooks/fueled-zsh/recipes/replace-shell.rb
@@ -1,10 +1,12 @@
-user node['current_user'] do
-  action :modify
-  shell '/bin/zsh'
-end
-
 zshrc_path = File.join(ENV['HOME'], '.zshrc')
 execute 'ensure .zshrc has correct owner' do
   command "chown " + (ENV['SUDO_USER'] || node['current_user']) + " #{zshrc_path}"
   only_if { FileTest.exists?(zshrc_path) }
 end
+
+user ENV['SUDO_USER'] || node['current_user'] do
+  action :modify
+  shell '/bin/zsh'
+end
+
+
