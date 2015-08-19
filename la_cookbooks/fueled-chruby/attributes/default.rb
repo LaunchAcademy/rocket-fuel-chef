fa6d2718068d88217adc9abe9ad945a1d96e67c4
Dan Pickett <dan.pickett@launchacademy.com>
2015-08-18 20:07:06 -0400

    upgrade ruby versions to latest patch levels

--- la_cookbooks/fueled-chruby/attributes/default.rb
+++ la_cookbooks/fueled-chruby/attributes/default.rb
@@ -1,7 +1,7 @@
 default['rocket-fuel']['chruby']['rubies']['ruby'] = [
   '1.9.3-p551',
-  '2.0.0-p598',
-  '2.2.1'
+  '2.0.0-p647',
+  '2.2.3'
 ]
 
-default['rocket-fuel']['chruby']['default_ruby'] = '2.2.1'
+default['rocket-fuel']['chruby']['default_ruby'] = '2.2.3'
