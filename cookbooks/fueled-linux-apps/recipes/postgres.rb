5789191f9df18f49050726cc1fa2a3016099c5c1
Dan Pickett <dan.pickett@launchacademy.com>
2015-12-23 22:54:47 -0500

    use postgres 9.4

--- cookbooks/fueled-linux-apps/recipes/postgres.rb
+++ cookbooks/fueled-linux-apps/recipes/postgres.rb
@@ -1,2 +1,14 @@
- package 'postgresql'
- package 'postgresql-contrib'
+apt_repository 'precise-pgdg' do
+ uri        'http://apt.postgresql.org/pub/repos/apt/'
+ components ['main']
+end
+
+package 'postgresql-9.4'
+package 'postgresql-contrib-9.4'
+
+ # create new postgres user
+execute "create new postgres user" do
+  user "postgres"
+  command "psql -c \"create user vagrant WITH SUPERUSER;\""
+  not_if { `sudo -u postgres psql -tAc \"SELECT * FROM pg_roles WHERE rolname='vagrant'\" | wc -l`.chomp == "1" }
+end
