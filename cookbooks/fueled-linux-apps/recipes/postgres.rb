a3e908973f63d8eb7fde877ba4e16cfd9ab55603
Dan Pickett <dan.pickett@launchacademy.com>
2015-12-26 16:42:23 -0500

    fix sql/command line syntax issues for postgres

--- cookbooks/fueled-linux-apps/recipes/postgres.rb
+++ cookbooks/fueled-linux-apps/recipes/postgres.rb
@@ -12,11 +12,11 @@ package 'postgresql-contrib-9.4'
 execute "create new postgres user" do
   user "postgres"
   command "psql -c \"create user #{ENV['SUDO_USER'] || node['current_user']} WITH SUPERUSER;\""
-  not_if { `sudo -u postgres psql -tAc \"SELECT * FROM pg_roles WHERE rolname='#{ENV['SUDO_USER'] || node['current_user']}'\" | wc -l`.chomp == "1" }
+  not_if { `sudo -u postgres psql -tAc \"SELECT * FROM pg_roles WHERE rolname='#{ENV['SUDO_USER'] || node['current_user']}';\" | wc -l`.chomp == "1" }
 end
 
 execute "create new postgres database" do
   user "postgres"
   command "psql -c \"CREATE DATABASE vagrant WITH owner #{ENV['SUDO_USER'] || node['current_user']};\""
-  not_if { `sudo -u postgres psql -tAc \"SELECT 1 FROM pg_database WHERE datname='#{ENV['SUDO_USER'] || node['current_user']}'\"; | wc -l`.chomp == "1"}
+  not_if { `sudo -u postgres psql -tAc \"SELECT 1 FROM pg_database WHERE datname='#{ENV['SUDO_USER'] || node['current_user']}';\" | wc -l`.chomp == "1"}
 end
