2f723fb445ab368a71898cf701a492e0bf288dbd
Dan Pickett <dan.pickett@launchacademy.com>
2016-03-02 16:16:45 -0500

    attempt at making more up to date node

--- cookbooks/fueled-linux-apps/recipes/node.rb
+++ cookbooks/fueled-linux-apps/recipes/node.rb
@@ -1,1 +1,7 @@
-include_recipe "nodejs::nodejs_from_source"
+apt_repository 'nodejs' do
+  uri          'https://deb.nodesource.com/node'
+  components   ['main']
+  key          'https://deb.nodesource.com/gpgkey/nodesource.gpg.key'
+end
+
+package 'nodejs'
