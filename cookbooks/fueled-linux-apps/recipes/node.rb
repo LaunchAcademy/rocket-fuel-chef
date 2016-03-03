bba71ed734746be086d91611a96f7e6547775752
Dan Pickett <dan.pickett@launchacademy.com>
2016-03-03 12:39:16 -0500

    separate out node commands

--- cookbooks/fueled-linux-apps/recipes/node.rb
+++ cookbooks/fueled-linux-apps/recipes/node.rb
@@ -1,10 +1,18 @@
-commands = [
-  'curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.0/install.sh | bash',
-  'source ~/.nvm/nvm.sh',
-  'nvm install v5'
-]
 nvm_path = File.join(File.join(ENV['HOME'], '.nvm'))
-execute "install node" do
-  command commands.join(" && ")
+
+execute 'install nvm' do
+  command 'curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.0/install.sh | bash'
   not_if { Dir.exist?(nvm_path) }
+end
+
+execute 'source nvm' do
+  command '. ~/.nvm/nvm.sh'
+end
+
+script 'install node via nvm' do
+  interpreter 'bash'
+  code <<-SCRIPT
+  source #{File.join(nvm_path, 'nvm.sh')}
+  nvm install v5
+  SCRIPT
 end
