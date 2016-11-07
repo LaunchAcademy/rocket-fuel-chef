00479c7bbba232abf0b5fa66f500a137776bbd5d
Dan Pickett <dan.pickett@launchacademy.com>
2016-11-07 07:18:34 -0500

    bump ruby versions

--- cookbooks/fueled-chruby/attributes/default.rb
+++ cookbooks/fueled-chruby/attributes/default.rb
@@ -1,6 +1,6 @@
 default['rocket-fuel']['chruby']['rubies']['ruby'] = [
-  '2.0.0-p647',
-  '2.2.4'
+  '2.0.0-p648',
+  '2.3.1'
 ]
 
-default['rocket-fuel']['chruby']['default_ruby'] = '2.2.4'
+default['rocket-fuel']['chruby']['default_ruby'] = '2.3.1'
