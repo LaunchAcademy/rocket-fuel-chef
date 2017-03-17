cb9ae11084776a7bf226d9c3903bb2a654452586
Dan Pickett <dan.pickett@launchacademy.com>
2017-03-17 18:51:32 -0400

    upgrade ruby versions

--- cookbooks/fueled-chruby/attributes/default.rb
+++ cookbooks/fueled-chruby/attributes/default.rb
@@ -1,6 +1,7 @@
 default['rocket-fuel']['chruby']['rubies']['ruby'] = [
   '2.0.0-p648',
-  '2.3.1'
+  '2.3.3',
+  '2.4.0'
 ]
 
-default['rocket-fuel']['chruby']['default_ruby'] = '2.3.1'
+default['rocket-fuel']['chruby']['default_ruby'] = '2.3.3'
