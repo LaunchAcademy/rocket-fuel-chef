db290c3da1d43eacde498287883bd958d7345455
Dan Pickett <dan.pickett@launchacademy.com>
2018-06-18 16:26:16 -0400

    bump ruby versions and chruby patch

--- la_cookbooks/fueled-chruby/recipes/chruby.rb
+++ la_cookbooks/fueled-chruby/recipes/chruby.rb
@@ -2,7 +2,7 @@ if platform_family?('mac_os_x')
   package 'chruby'
 else
   chruby_path = "#{Chef::Config[:file_cache_path]}/chruby.tar.gz"
-  version = '0.3.8'
+  version = '0.3.9'
   remote_file chruby_path do
     source "https://github.com/postmodern/chruby/archive/v#{version}.tar.gz"
     action :create_if_missing
