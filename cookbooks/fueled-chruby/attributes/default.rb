2139155b05344df7cfe9d90c482f76422066788d
Dan Pickett <dan.pickett@launchacademy.com>
2018-08-27 10:37:46 -0400

    updates

--- cookbooks/fueled-chruby/attributes/default.rb
+++ cookbooks/fueled-chruby/attributes/default.rb
@@ -1,6 +1,7 @@
 default['rocket-fuel']['chruby']['rubies']['ruby'] = [
-  '2.3.3',
-  '2.4.2'
+  '2.3.7',
+  '2.4.4',
+  '2.5.1'
 ]
 
-default['rocket-fuel']['chruby']['default_ruby'] = '2.3.3'
+default['rocket-fuel']['chruby']['default_ruby'] = '2.3.7'
